e756bc0e49b281490deb317f5673fbc911802fe4
parhanhambali <parhanhambali17@gmail.com>
2023-02-20 12:25:02 +0700

    create custom bottom navigation

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:toko_online/pages/splash_page.dart';
 import 'package:toko_online/pages/sign_in_page.dart';
 import 'package:toko_online/pages/sign_up_page.dart';
+import 'pages/home/main_page.dart';
 import 'theme.dart';
 
 void main() => runApp(MyApp());
@@ -15,7 +16,8 @@ class MyApp extends StatelessWidget {
       routes: {
         '/' : (context) => SplashPage(),
         '/sign-in': (context) => SignInPage(),
-        '/sign-up' : (context) => SignUpPage()
+        '/sign-up' : (context) => SignUpPage(),
+        '/home': (context) => MainPage(),
       },
     );
   }
